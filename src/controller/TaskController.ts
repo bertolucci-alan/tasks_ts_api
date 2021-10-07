f6fd93f3ca43ad96a17c7fded27c3c03fdc5685d
bertolucci-alan <beterrababir23@gmail.com>
2021-10-07 00:47:42 -0300

    feat: CRUD tasks

---
+++ src/controller/TaskController.ts
@@ -0,0 +1,57 @@
+import { getRepository } from "typeorm";
+import { Tasks } from "../entity/Task";
+import { request, Request, response, Response } from "express";
+
+export const getTasks = async (request: Request, response: Response) => {
+    const tasks = await getRepository(Tasks).find();
+    return response.json(tasks);
+}
+
+export const showTasks = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const task = await getRepository(Tasks).findOne(id);
+    return response.json(task);
+}
+
+export const saveTasks = async (request: Request, response: Response) => {
+    const tasks = await getRepository(Tasks).save(request.body);
+    return response.json(tasks);
+}
+
+export const updateTasks = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const task = await getRepository(Tasks).update(id, request.body);
+
+    if(task.affected === 1) {
+        const taskUpdate = await getRepository(Tasks).findOne(id);
+        return response.json(taskUpdate);
+    }
+
+    return response.json({"error": "Task not found!"}).status(404);
+}
+
+export const finishTasks = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const task = await getRepository(Tasks).update(id, {
+        finish: true
+    });
+
+    if(task.affected === 1) {
+        const taskFinish = await getRepository(Tasks).findOne(id);
+        return response.json({message: "Task finished" });
+    }
+
+    return response.json({"error": "Task not found!"}).status(404);
+}
+
+export const deleteTask = async (request: Request, response: Response) => {
+    const { id } = request.params;
+    const task = await getRepository(Tasks).delete(id);
+
+    if(task.affected === 1) {
+        const taskDelete = await getRepository(Tasks).findOne(id);
+        return response.json({message: "Task deleted" });
+    }
+
+    return response.json({"error": "Task not found!"}).status(404);
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,20 @@
 import { Router, Request, Response } from "express";
 
+import { deleteTask, finishTasks, getTasks, saveTasks, showTasks, updateTasks } from "./controller/TaskController";
+
 const routes = Router();
 
-routes.get('/', (request: Request ,response: Response) => {
+routes.get('/a', (request: Request ,response: Response) => {
     return response.json({message: "Hello World!"});
 });
+
+routes.get('/tasks', getTasks);
+routes.get('/tasks/:id', showTasks);
+routes.post('/task-save', saveTasks);
+routes.put('/task-update/:id', updateTasks);
+routes.put('/task-finish/:id', finishTasks);
+routes.delete('/task-delete/:id', deleteTask);
+
+
 
 export default routes;
